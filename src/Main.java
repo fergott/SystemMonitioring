dd4c7d9ba6c53a2eb7974cd43aef2608ac972370
fergot <ffergot@gmail.com>
2026-09-17 12:10:49 +0100

    now provides desired console output

--- src/Main.java
+++ src/Main.java
@@ -7,24 +7,34 @@ void main() {
             (OperatingSystemMXBean)
                     ManagementFactory.getOperatingSystemMXBean();
     while (true) {
-    double cpu = os.getCpuLoad() * 100;
+        double cpu = os.getCpuLoad() * 100;
 
-    long totalMemory = os.getTotalMemorySize();
-    long freeMemory = os.getFreeMemorySize();
-    long usedMemory = totalMemory - freeMemory;
+        long totalMemory = os.getTotalMemorySize() / 1000000;
+        long freeMemory = os.getFreeMemorySize()/ 1000000;
+        long usedMemory = totalMemory - freeMemory;
 
         double ramUsage = (double) usedMemory / totalMemory * 100;
 
         double load = os.getSystemLoadAverage();
-
+        System.out.print("\033[H\033[2J");
         System.out.flush();
 
-        System.out.println("Cpu Usage: " + cpu);
-        System.out.println("total Memory Usage: " + totalMemory);
-        System.out.println("Memory used: " + usedMemory);
-        System.out.printf("percent of ram used %.2f \n", ramUsage);
-        System.out.printf("total load %.2f", load);
-
-        Thread.sleep(1000);
+       // Runtime.getRuntime().exec("clear");
+        System.out.printf("Cpu Usage: %f %% \n", cpu);
+        System.out.printf("total Memory Usage: %d \n",  totalMemory);
+        System.out.printf("Memory used: %d mb \n", usedMemory);
+        System.out.printf("Available memory: %d mb \n", freeMemory);
+        System.out.printf("ram usage %.2f %%\n", ramUsage);
+        System.out.printf("Average number of executable processes %.2f \n", load);
+        try
+        {
+            Thread.sleep(2000);
+        }
+        catch (InterruptedException e)
+        {
+            Thread.currentThread().interrupt(); // restore interrupted status
+        }
     }
+
+
 }
